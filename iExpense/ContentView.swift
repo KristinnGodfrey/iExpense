ea87a0f332a7d3c7d01eed0a786d4f6b7668a0b4
kristinngodfrey <kristinngodfrey@gmail.com>
2020-09-05 19:24:58 +0000

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Kristinn Godfrey on 05/09/2020.
+//  Copyright © 2020 me.kristinn.godfrey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
