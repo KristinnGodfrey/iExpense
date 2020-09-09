068f13c9ae1b4def22987ce7db195ce0a4ee030c
kristinngodfrey <kristinngodfrey@gmail.com>
2020-09-09 23:16:26 +0000

    fix readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # iExpense
+
+
+## Project Details
+This is my eighth Swift project, code can be found in [ContentView.swift](https://github.com/KristinnGodfrey/iExpense/blob/master/iExpense/ContentView.swift)
+                                                                        
+
+## Simulator
+<p align="left">
+  <img src="img/p1.png" width="30%" /> 
+</p>
+<p align="left">
+  <img src="img/p2.png" width="30%" /> 
+  <img src="img/p3.png" width="30%" /> 
+</p>
--- SwiftUIView.swift
+++ SwiftUIView.swift
@@ -8,14 +8,33 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
+struct AddView: View {
+    @ObservedObject var expenses: Expenses
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+
+    static let types = ["Business", "Personal"]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add new expense")
+        }
     }
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIView()
+        AddView()
     }
 }
