3dd2913d4c592311c09266fe321fc7c652a0c73a
kristinngodfrey <kristinngodfrey@gmail.com>
2020-09-09 23:10:45 +0000

    first commit

---
+++ AddView.swift
@@ -0,0 +1,21 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Kristinn Godfrey on 09/09/2020.
+//  Copyright © 2020 me.kristinn.godfrey. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView()
+    }
+}
---
+++ SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  iExpense
+//
+//  Created by Kristinn Godfrey on 09/09/2020.
+//  Copyright © 2020 me.kristinn.godfrey. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
